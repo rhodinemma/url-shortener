e5d661771158d1d016833d47d54d6431c661ec94
rhodinemma <rhodinemma10@gmail.com>
2023-06-14 13:08:50 +0300

    feat: add more functionality to url service - delete/update

--- src/services/urls.ts
+++ src/services/urls.ts
@@ -14,7 +14,7 @@ export const createShortURL = async (body: { url: string, id?: string }, user_id
 
     const results = await knex("urls").insert({ url: body.url, id: body.id, user_id }, "*");
 
-    return results;
+    return results[0];
 }
 
 export const resolveURL = async (id: string) => {
@@ -25,4 +25,44 @@ export const resolveURL = async (id: string) => {
     }
 
     return foundUrl.url;
+}
+
+export const updateURL = async (id: string, body: { url: string }, user_id: number) => {
+    if (!body.url) {
+        throw new Error("URL is required")
+    }
+
+    const foundUserUrl = await knex("urls").where({ id }).select(["user_id"]).first();
+
+    if (!foundUserUrl) {
+        throw new Error("URL is not found")
+    }
+
+    if (foundUserUrl.user_id !== user_id) {
+        throw new Error("You don't have permissions to update this URL")
+    }
+
+    const results = await knex("urls").where({ id }).update({ url: body.url }, "*")
+
+    return results[0]
+}
+
+export const deleteURL = async (id: string, user_id: number) => {
+    const foundUserUrl = await knex("urls").where({ id }).select(["user_id"]).first();
+
+    if (!foundUserUrl) {
+        throw new Error("URL is not found")
+    }
+
+    if (foundUserUrl.user_id !== user_id) {
+        throw new Error("You don't have permissions to delete this URL")
+    }
+
+    await knex("urls").where({ id }).delete();
+    return true;
+}
+
+export const getURLS = async (user_id: number, limit: number, offset: number) => {
+    const results = await knex("urls").where({ user_id }).limit(limit || 15).offset(offset || 0);
+    return results;
 }
